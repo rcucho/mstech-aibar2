bd7a4fd5dce0bf8e830e6939c5c6273b1af8eb1b
rcucho <38509900+rcucho@users.noreply.github.com>
2021-04-19 18:20:51 -0500

    Update sale_order.py

--- mstech_aibar/models/sale_order.py
+++ mstech_aibar/models/sale_order.py
@@ -11,9 +11,7 @@ class SaleOrder(models.Model) :
     
     @api.depends('partner_quotation')
     def _onchange_count_part_qu(self):
-        if partner_quotation:
-            for rec in self:
-                rec.count_part_qu = len(self.partner_quotation)
+        self.count_part_qu = len(self.partner_quotation)
    
     
     
