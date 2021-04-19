6467f2db19eb4d67cb2295dd6a4d5bfa5763c63a
rcucho <38509900+rcucho@users.noreply.github.com>
2021-04-19 17:42:07 -0500

    Update sale_order.py

--- mstech_aibar/models/sale_order.py
+++ mstech_aibar/models/sale_order.py
@@ -6,11 +6,11 @@ from odoo.exceptions import ValidationError, UserError, Warning
 
 class SaleOrder(models.Model) :
     _inherit = 'sale.order'
-    partner_quotation = fields.One2many(comodel_name='res.partner', inverse_name='sale_order_ids', string='Historial')
+    partner_quotation = fields.One2many(comodel_name='res.partner', inverse_name='sale_order_ids', string='Historial', compute='_onchange_count_part_qu')
     count_part_qu=fields.Integer(string='Cotizaciones Anteriores')
     
     @api.onchange('count_partner_quotation')
-    def onchange_count_part_qu(self):
+    def _onchange_count_part_qu(self):
         if count_partner_quotation:
             self.count_part_qu = len(self.partner_quotation)
     
